adab089eb57c94e901cff2c60565ce3fb89b0f1d
Arvind <europa1613@users.noreply.github.com>
2018-07-01 14:00:22 -0500

    Initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/ping', function(req, res) {
-	res.end('pong');
+    res.end('pong');
 });
 
 app.post('/jokes/chunked', function(req, res) {
@@ -21,22 +21,33 @@ app.get('/jokes/chunked', function(req, res) {
 
 function prepareChunkedResponse(req, res) {
     return getJokes(100).then(function(response) {
+        
         var jokesResponse = JSON.parse(response);
         var jokes = jokesResponse.value;
+
         //console.log(jokesResponse.type);
+
         for (var i = 0; i <= jokes.length; i++) {
+
             (function(numberOfJokes) {
+
                 setTimeout(function() {
+
                     //res.write(JSON.stringify(jokes));
+
                     //console.log(numberOfJokes + '|' + JSON.stringify(jokes[numberOfJokes]));
+
                     if (jokes[numberOfJokes]) {
                         res.write(JSON.stringify(jokes[numberOfJokes]));
                         res.flushHeaders();
                     }
+
                     if (numberOfJokes === jokes.length) {
                         res.end();
                     }
+
                 }, numberOfJokes * 1000);
+
             })(i)
         }
     }).catch(function(error) {
@@ -46,13 +57,17 @@ function prepareChunkedResponse(req, res) {
 
 function getJokes(numberOfJokes) {
     return new Promise((resolve, reject) => {
+        
         request.get({ url: 'http://api.icndb.com/jokes/random/' + numberOfJokes }, function(error, response, body) {
+            
             if (!error && response.statusCode == 200) {
                 resolve(body);
             } else {
                 reject(error);
             }
+
         });
+        
     });
 }
 
